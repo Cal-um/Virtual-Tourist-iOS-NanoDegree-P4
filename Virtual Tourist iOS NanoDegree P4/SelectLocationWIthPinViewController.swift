dd73c8dcab82b3ff3ccaea1bfc230693b54ae856
Calum Harris <harris.calum@gmail.com>
2016-05-19 10:52:42 +0100

    Initial commit

---
+++ Virtual Tourist iOS NanoDegree P4/SelectLocationWIthPinViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SelectLocationWIthPinViewController.swift
+//  Virtual Tourist iOS NanoDegree P4
+//
+//  Created by Calum Harris on 19/05/2016.
+//  Copyright © 2016 Calum Harris. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class SelectLocationWithPinViewController: UIViewController {
+
+  @IBOutlet weak var mapView: MKMapView!
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
