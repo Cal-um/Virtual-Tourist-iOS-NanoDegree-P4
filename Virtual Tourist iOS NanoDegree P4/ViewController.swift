16a140e00cab26c7bd628ded138b545dbc10fed6
Calum Harris <harris.calum@gmail.com>
2016-05-19 08:42:17 +0100

    Initial Commit

---
+++ Virtual Tourist iOS NanoDegree P4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Virtual Tourist iOS NanoDegree P4
+//
+//  Created by Calum Harris on 19/05/2016.
+//  Copyright © 2016 Calum Harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ Virtual Tourist iOS NanoDegree P4Tests/Virtual_Tourist_iOS_NanoDegree_P4Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Virtual_Tourist_iOS_NanoDegree_P4Tests.swift
+//  Virtual Tourist iOS NanoDegree P4Tests
+//
+//  Created by Calum Harris on 19/05/2016.
+//  Copyright © 2016 Calum Harris. All rights reserved.
+//
+
+import XCTest
+@testable import Virtual_Tourist_iOS_NanoDegree_P4
+
+class Virtual_Tourist_iOS_NanoDegree_P4Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
